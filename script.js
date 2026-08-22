a3e8b9c498ee7836ec44ee83b8e92842c13af559
Mahi-Haque <sheikhmahidul37@gmail.com>
2026-08-22 14:33:38 +0600

    Js Basic

---
+++ script.js
@@ -0,0 +1,41 @@
+// Variables        
+var num1 = "Hello"; // String 
+console.log(num1);
+let num2 = true;    // Boolean
+console.log(num2);
+let num3 = 150;     // Numbers
+console.log(num3);
+const pi = 3.1416;  //  Float
+console.log(pi);
+
+// = assignment operator 
+
+alert("Hello JavaScript");
+
+let name1 = "Mahidul Haque"
+console.log(name1);
+
+// Voting age
+let age1 = 17;
+
+if(18 >= age1) {
+    console.log("He/She is eligible to vote");
+} else {
+    console.log("He/She is not old enough to vote");
+}
+
+// Marriage Age
+let male_age = 21;
+let female_age = 18;
+
+if (male_age >= 21) {
+    console.log("He meets the minimum age requirement for marriage.")
+} else {
+    console.log("He is too young to get married under the law.")
+}
+
+if (female_age >= 18) {
+    console.log("She meets the minimum age requirement for marriage.")
+} else {
+    console.log("She is too young to get married under the law.")
+}
